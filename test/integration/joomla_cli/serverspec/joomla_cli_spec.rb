5aca56077cd424a3ef4b80a4307e8a36ee1ec191
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-03-13 11:59:22 +0000

    Fix variable cli name.

--- test/integration/joomla_cli/serverspec/joomla_cli_spec.rb
+++ test/integration/joomla_cli/serverspec/joomla_cli_spec.rb
@@ -5,9 +5,9 @@ set :backend, :exec
 
 describe 'joomla cli tool' do
 
-    wp_cli = '/usr/local/bin/joomla'
+    joomla_cli = '/usr/local/bin/joomla'
 
-    describe file(wp_cli) do
+    describe file(joomla_cli) do
         it { should exist }
         it { should be_mode 740 }
         it { should be_owned_by 'www-data' }
