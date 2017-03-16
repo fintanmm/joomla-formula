3ba32a7257d2dbeeabf74607dca288a07d9a7310
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-03-16 12:31:16 +0000

    Fixed test for cli. Test if symlink is created.

--- test/integration/joomla_cli/serverspec/joomla_cli_spec.rb
+++ test/integration/joomla_cli/serverspec/joomla_cli_spec.rb
@@ -5,11 +5,16 @@ set :backend, :exec
 
 describe 'joomla cli tool' do
 
-    joomla_cli = '/usr/local/bin/joomla'
+    joomla_cli_link = '/usr/local/bin/joomla'
+    joomla_cli = '/usr/local/bin/vendor/joomlatools/console/bin/joomla'
 
+    describe file(joomla_cli_link) do
+        it { should be_symlink }
+    end
+    
     describe file(joomla_cli) do
         it { should exist }
-        it { should be_mode 740 }
+        it { should be_mode 755 }
         it { should be_owned_by 'www-data' }
         it { should be_grouped_into 'www-data' }
     end
